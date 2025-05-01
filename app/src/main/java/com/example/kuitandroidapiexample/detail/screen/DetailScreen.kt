12f0e0e0ea8c6738d84074c1f58757597e6a6b13
yskim6772 <yskim6772@gmail.com>
2025-05-01 15:42:15 +0900

    [feat] : 삭제하기 버튼 추가

--- app/src/main/java/com/example/kuitandroidapiexample/detail/screen/DetailScreen.kt
+++ app/src/main/java/com/example/kuitandroidapiexample/detail/screen/DetailScreen.kt
@@ -1,10 +1,13 @@
 package com.example.kuitandroidapiexample.detail.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,6 +25,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -119,17 +123,35 @@ fun DetailScreen(
                     style = typography.semiBold.copy(fontSize = 14.sp),
                     modifier = Modifier.align(Alignment.BottomStart)
                 )
-
-
             }
-            Text(
-                text = "신고자 : 조익성",
-                style = typography.semiBold.copy(fontSize = 14.sp),
-                modifier = Modifier.padding(start = 40.dp, top = 21.dp)
-            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "신고자 : 조익성",
+                    style = typography.semiBold.copy(fontSize = 14.sp),
+                    modifier = Modifier.padding(start = 40.dp, top = 21.dp)
+                )
+                Box(
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(colors.red)
+                        .padding(10.dp),
+                ) {
+                    Text(
+                        text = "삭제하기",
+                        style = typography.semiBold.copy(fontSize = 20.sp),
+                        color = colors.white,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
         }
-
-
     }
 }
 
