be1fe3a106667b5dd1e9f75f9aa8fd7d1ae274f1
chohs4164 <josh100as@gmail.com>
2025-05-06 01:00:46 +0900

    feat : AnimalService HTTP 메소드 관련 함수의 suspend 키워드 추가

--- app/src/main/java/com/example/kuitandroidapiexample/data/service/AnimalService.kt
+++ app/src/main/java/com/example/kuitandroidapiexample/data/service/AnimalService.kt
@@ -12,20 +12,20 @@ import retrofit2.http.Path
 
 interface AnimalService {
     @GET("animals")
-    fun getTotalAnimalList() : Call<ResponseAnimalListDto>
+    suspend fun getTotalAnimalList() : Call<ResponseAnimalListDto>
 
     @GET("animals/{id}")
-    fun getAnimalDetail(
+    suspend fun getAnimalDetail(
         @Path("id") id: Int
     ) : Call<ResponseAnimalDetailDto>
 
     @POST("animals")
-    fun postAddAnimal(
+    suspend fun postAddAnimal(
         @Body request: RequestAddAnimalDto
     ) : Call<Unit>
 
     @DELETE("animals/{id}")
-    fun deleteAnimal(
+    suspend fun deleteAnimal(
         @Path("id") id: Int
     ) : Call<Unit>
 }
