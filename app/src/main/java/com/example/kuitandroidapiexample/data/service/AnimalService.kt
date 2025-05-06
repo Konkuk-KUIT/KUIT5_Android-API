c506761ad8bbb05e8c1890335a78f90f88f936b4
Bb-jh <203391596+Bb-jh@users.noreply.github.com>
2025-05-07 02:07:49 +0900

    AnimalService HTTP 메소드 관련 함수에 suspend 키워드 추가

--- app/src/main/java/com/example/kuitandroidapiexample/data/service/AnimalService.kt
+++ app/src/main/java/com/example/kuitandroidapiexample/data/service/AnimalService.kt
@@ -3,7 +3,6 @@ package com.example.kuitandroidapiexample.data.service
 import com.example.kuitandroidapiexample.data.dto.request.RequestAddAnimalDto
 import com.example.kuitandroidapiexample.data.dto.response.ResponseAnimalDetailDto
 import com.example.kuitandroidapiexample.data.dto.response.ResponseAnimalListDto
-import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
