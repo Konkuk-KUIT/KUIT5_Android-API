2d2266cfcf606c4863a9d4635684173b82c6f3ed
hyungyu-02 <yona20020209@gmail.com>
2025-05-07 02:28:14 +0900

    feat: ResponseDeleteAnimalDto 서버 응답에 맞게 수정

--- app/src/main/java/com/example/kuitandroidapiexample/data/dto/response/ResponseDeleteAnimalDto.kt
+++ app/src/main/java/com/example/kuitandroidapiexample/data/dto/response/ResponseDeleteAnimalDto.kt
@@ -4,5 +4,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ResponseDeleteAnimalDto(
+    val message: String,
+    val data: DeleteAnimalData
+)
+
+@Serializable
+data class DeleteAnimalData(
     val id: Int
 )
