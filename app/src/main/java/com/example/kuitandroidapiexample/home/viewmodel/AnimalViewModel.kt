a25fa3573452ab4ce6162a69664bb52aa747e4f4
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-05-09 01:03:14 +0900

    feat: week6 완료

--- app/src/main/java/com/example/kuitandroidapiexample/home/viewmodel/AnimalViewModel.kt
+++ app/src/main/java/com/example/kuitandroidapiexample/home/viewmodel/AnimalViewModel.kt
@@ -20,8 +20,8 @@ class AnimalViewModel : ViewModel() {
     private val _animalListState = mutableStateOf<BaseResponse<List<ResponseAnimalDto>>?>(null)
     val animalListState: State<BaseResponse<List<ResponseAnimalDto>>?> get() = _animalListState
 
-    private val _animalDetailState = mutableStateOf<ResponseAnimalDetailDto?>(null)
-    val animalDetailState: State<ResponseAnimalDetailDto?> get() = _animalDetailState
+    private val _animalDetailState = mutableStateOf<BaseResponse<ResponseAnimalDetailDto>?>(null)
+    val animalDetailState: State<BaseResponse<ResponseAnimalDetailDto>?> get() = _animalDetailState
 
     private val _addAnimalState = mutableStateOf<Boolean?>(null)
     val addAnimalState: State<Boolean?> get() = _addAnimalState
@@ -33,109 +33,51 @@ class AnimalViewModel : ViewModel() {
         viewModelScope.launch {
             runCatching {
                 animalService.getTotalAnimalList()
+            }.onSuccess { data ->
+                _animalListState.value = data
+            }.onFailure { error ->
+                Log.e("getTotalAnimalList", error.message ?: "Unknown error")
             }
-                .onSuccess { data ->
-                    _animalListState.value = data
-                }
-                .onFailure { error ->
-                    Log.e("getTotalAnimalList", error.message ?: "Unknown error")
-                }
         }
-//            .enqueue(object : Callback<ResponseAnimalListDto> {
-//                override fun onResponse(
-//                    call: Call<ResponseAnimalListDto>,
-//                    response: Response<ResponseAnimalListDto>
-//                ) {
-//                    if (response.isSuccessful) {
-//                        _animalListState.value = response.body()
-//                    } else {
-//                        Log.e("getTotalAnimalList", "${response.code()} ${response.message()}")
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<ResponseAnimalListDto>, t: Throwable) {
-//                    Log.e("getTotalAnimalList", "서버 통신 오류: ${t.message}")
-//                }
-//            })
     }
 
     fun getAnimalDetail(id: Int) {
         viewModelScope.launch {
-            animalService.getAnimalDetail(id)
+            runCatching {
+                animalService.getAnimalDetail(id)
+            }.onSuccess { data ->
+                _animalDetailState.value = data
+            }.onFailure { error ->
+                Log.e("getAnimalDetail", error.message ?: "Unknown error")
+            }
         }
-//            .enqueue(object : Callback<ResponseAnimalDetailDto> {
-//                override fun onResponse(
-//                    call: Call<ResponseAnimalDetailDto>,
-//                    response: Response<ResponseAnimalDetailDto>
-//                ) {
-//                    if (response.isSuccessful) {
-//                        _animalDetailState.value = response.body()
-//                    } else {
-//                        Log.e("getAnimalDetail", "${response.code()} ${response.message()}")
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<ResponseAnimalDetailDto>, t: Throwable) {
-//                    Log.e("getAnimalDetail", "서버 통신 오류: ${t.message}")
-//                }
-//            })
     }
 
     fun postAddAnimal(request: RequestAddAnimalDto) {
         viewModelScope.launch {
-            animalService.postAddAnimal(request)
+            runCatching {
+                animalService.postAddAnimal(request)
+            }.onSuccess { data ->
+                _addAnimalState.value = true
+            }.onFailure { error ->
+                Log.e("postAddAnimal", error.message ?: "Unknown error")
+            }
         }
-//            .enqueue(object: Callback<Unit> {
-//                override fun onResponse(
-//                    call: Call<Unit>,
-//                    response: Response<Unit>
-//                ) {
-//                    if (response.isSuccessful) {
-//                        _addAnimalState.value = true
-//                    } else {
-//                        Log.e("postAddAnimal", "${response.code()} ${response.message()}")
-//                        _addAnimalState.value = false
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<Unit>, t: Throwable) {
-//                    Log.e("postAddAnimal", "서버 통신 오류: ${t.message}")
-//                }
-//            })
     }
 
     fun deleteAnimal(id: Int) {
         viewModelScope.launch {
             runCatching {
-                // TODO : 삭제 함수 호출
                 animalService.deleteAnimal(id)
             }.fold(
                 onSuccess = { data ->
-                    // TODO : Data 핸들링
+                    _deleteAnimalState.value = true
                 },
                 onFailure = { error ->
-                    // TODO : Error Handling
+                    Log.e("deleteAnimal", error.message ?: "Unknown error")
                 }
-
             )
         }
-//            .enqueue(object : Callback<Unit> {
-//                override fun onResponse(
-//                    call: Call<Unit>,
-//                    response: Response<Unit>
-//                ) {
-//                    if (response.isSuccessful) {
-//                        _deleteAnimalState.value = true
-//                    } else {
-//                        Log.e("deleteAnimal", "${response.code()} ${response.message()}")
-//                        _deleteAnimalState.value = false
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<Unit>, t: Throwable) {
-//                    Log.e("deleteAnimal", "서버 통신 오류: ${t.message}")
-//                }
-//            })
     }
 
     fun addAnimal(
