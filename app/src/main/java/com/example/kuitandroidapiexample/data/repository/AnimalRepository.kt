39e85524c021ab962c32a1d8d686bcbf84e159a2
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-05-09 01:24:31 +0900

    feat: AnimalRepository 생성 및 getAnimal 함수 구현

---
+++ app/src/main/java/com/example/kuitandroidapiexample/data/repository/AnimalRepository.kt
@@ -0,0 +1,9 @@
+package com.example.kuitandroidapiexample.data.repository
+
+import com.example.kuitandroidapiexample.data.service.AnimalService
+
+class AnimalRepository(
+    private val animalService: AnimalService
+) {
+    suspend fun getAnimal(id: Int) = runCatching { animalService.getAnimalDetail(id) }
+}
