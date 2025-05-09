4985c9382aab8daf3d59400c89855882666b4f94
librawish808 <sophiadiary842@gmail.com>
2025-05-10 01:27:02 +0900

    feat: AnimalService HTTP 메소드 관련함수에 suspend 키워드 추가

--- app/src/main/java/com/example/kuitandroidapiexample/data/service/AnimalService.kt
+++ app/src/main/java/com/example/kuitandroidapiexample/data/service/AnimalService.kt
@@ -3,7 +3,6 @@ package com.example.kuitandroidapiexample.data.service
 import com.example.kuitandroidapiexample.data.dto.request.RequestAddAnimalDto
 import com.example.kuitandroidapiexample.data.dto.response.ResponseAnimalDetailDto
 import com.example.kuitandroidapiexample.data.dto.response.ResponseAnimalListDto
-import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
@@ -12,20 +11,20 @@ import retrofit2.http.Path
 
 interface AnimalService {
     @GET("animals")
-    fun getTotalAnimalList() : Call<ResponseAnimalListDto>
+    suspend fun getTotalAnimalList() : ResponseAnimalListDto
 
     @GET("animals/{id}")
-    fun getAnimalDetail(
+    suspend fun getAnimalDetail(
         @Path("id") id: Int
-    ) : Call<ResponseAnimalDetailDto>
+    ) : ResponseAnimalDetailDto
 
     @POST("animals")
-    fun postAddAnimal(
+    suspend fun postAddAnimal(
         @Body request: RequestAddAnimalDto
-    ) : Call<Unit>
+    )
 
     @DELETE("animals/{id}")
-    fun deleteAnimal(
+    suspend fun deleteAnimal(
         @Path("id") id: Int
-    ) : Call<Unit>
+    )
 }
