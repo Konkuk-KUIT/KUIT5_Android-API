5a0be5d983371ba4b1551c27ae49c253332b6077
Bb-jh <203391596+Bb-jh@users.noreply.github.com>
2025-05-14 04:57:04 +0900

    feat: AnimalRepository 생성 및 getAnimal 함수 구현

---
+++ app/src/main/java/com/example/kuitandroidapiexample/data/repository/AnimalRepository.kt
@@ -0,0 +1,13 @@
+package com.example.kuitandroidapiexample.data.repository
+
+import com.example.kuitandroidapiexample.data.service.AnimalService
+
+class AnimalRepository(
+    private val animalService: AnimalService
+) {
+    suspend fun getAnimal(id: Int) = runCatching { animalService.getAnimalDetail(id) }
+
+//    suspend fun getAnimals
+
+//    suspend fun postAnimal(animal: Animal)
+}
